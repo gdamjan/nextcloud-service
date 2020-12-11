0c227ac2a9dc0132e3fab81cf2a48f347a246e87
Дамјан Георгиевски <gdamjan@gmail.com>
2020-12-11 19:58:02 +0100

    bump nextcloud to 20.0.3

--- nextcloud.nix
+++ nextcloud.nix
@@ -25,8 +25,8 @@ in
 
 pkgs.stdenv.mkDerivation rec {
   pname = "nextcloud";
-  version = "20.0.1";
-  sha256 = "96c6b50e1e676e46cec82d8f8ff1abd5eef9dfa00580081b6c640612c3ff2efc";
+  version = "20.0.3";
+  sha256 = "e0f64504d338f64d3c677357f0012cf8b0ed0dc42ec08f958b6dc4ff70edf175";
 
   src = pkgs.fetchurl {
     url = "https://github.com/nextcloud/server/releases/download/v${version}/nextcloud-${version}.tar.bz2";

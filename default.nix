355379f8ad9e675e0eeb4b23ed82f195243d1235
Дамјан Георгиевски <gdamjan@gmail.com>
2022-01-10 22:32:47 +0100

    also create /var/log/nextcloud
    since 40bbe365d6f3f9899d76dd40380967352ad6f434 log file goes there
    must exist, systemd will bind mount it to /var/log/private/nextcloud on
    the host

--- default.nix
+++ default.nix
@@ -57,7 +57,7 @@ let
         ln -s ${nextcloud} $out/srv/nextcloud
 
         # create empty directories as mount points for the services
-        mkdir -p $out/var/lib/nextcloud $out/etc/ssl/certs
+        mkdir -p $out/var/lib/nextcloud $out/var/log/nextcloud $out/etc/ssl/certs
         substituteAll ${./files/nextcloud.service.in} $out/etc/systemd/system/nextcloud.service
         substituteAll ${./files/nextcloud-cron.service.in} $out/etc/systemd/system/nextcloud-cron.service
         substituteAll ${./files/nextcloud-first-run.service.in} $out/etc/systemd/system/nextcloud-first-run.service

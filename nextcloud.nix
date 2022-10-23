3254ba763b6c2689ca3f3473021527098f94a38b
Дамјан Георгиевски <gdamjan@gmail.com>
2022-10-23 18:30:45 +0200

    Update nextcloud.nix

--- nextcloud.nix
+++ nextcloud.nix
@@ -1,14 +1,14 @@
 { pkgs }:
 let
   nextcloud = rec {
-    version = "24.0.6";
+    version = "25.0.0";
     url = "https://download.nextcloud.com/server/releases/nextcloud-${version}.tar.bz2";
     sha256 = "sha256:0083mydqw0lfd4rcqgmh2ww9mx87gvwbpz8551r7wzm4h2czyvdj";
   };
   apps = [
     rec {
       name = "spreed";
-      version = "14.0.5";
+      version = "15.0.0";
       url = "https://github.com/nextcloud-releases/${name}/releases/download/v${version}/${name}-v${version}.tar.gz";
       sha256 = "sha256:1nwxkb3zrn1ygavxsrzr8mqz8njpcn2v0f71sjrjcn0v2xi023n2";
     }

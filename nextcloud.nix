c301bda8aff061c965b1c6d3eea8fb0cf74fdd99
Дамјан Георгиевски <gdamjan@gmail.com>
2023-02-25 16:40:09 +0100

    nextcloud 25.0.4, spreed 15.0.4

--- nextcloud.nix
+++ nextcloud.nix
@@ -1,16 +1,16 @@
 { pkgs }:
 let
   nextcloud = rec {
-    version = "25.0.3";
+    version = "25.0.4";
     url = "https://download.nextcloud.com/server/releases/nextcloud-${version}.tar.bz2";
-    sha256 = "sha256:1v00bji3wp9q3flcgh879siqgbacq9hlzqkg15lj9ybffcii8asb";
+    sha256 = "sha256:19m1d7l0z6vh8vvhzcr6lxwjl21k7cpkb2wqsvi06hx9hc01w9f3";
   };
   apps = [
     rec {
       name = "spreed";
-      version = "15.0.3";
+      version = "15.0.4";
       url = "https://github.com/nextcloud-releases/${name}/releases/download/v${version}/${name}-v${version}.tar.gz";
-      sha256 = "sha256:016yl2w8fla3mxkykrnb4nlsjciayikgrxwqp5qfzz6jycsl6kyj";
+      sha256 = "sha256:10ss1c7sb28f62mykz9xlk8gb2r8d56919wvq87650v71yqyqb98";
     }
   ];
 
@@ -20,12 +20,9 @@ pkgs.stdenv.mkDerivation rec {
   pname = "nextcloud";
   version = nextcloud.version;
 
-  src = builtins.fetchurl {
-    url = nextcloud.url;
-    sha256 = nextcloud.sha256;
-  };
+  src = builtins.fetchurl { inherit (nextcloud) url sha256; };
 
-  additionalApps = map ({url, sha256, ...}: builtins.fetchurl {inherit url sha256;}) apps;
+  additionalApps = map ({url, sha256, ...}: builtins.fetchurl { inherit url sha256; }) apps;
 
   installPhase = ''
     runHook preInstall

b64e373cda88c50c99715ebfca0d3dac5199aa74
Дамјан Георгиевски <gdamjan@gmail.com>
2023-05-26 15:32:53 +0200

    nextcloud 26.0.1, spreed 16.0.4

--- nextcloud.nix
+++ nextcloud.nix
@@ -1,16 +1,16 @@
 { pkgs }:
 let
   nextcloud = rec {
-    version = "26.0.0";
+    version = "26.0.1";
     url = "https://download.nextcloud.com/server/releases/nextcloud-${version}.tar.bz2";
-    sha256 = "sha256:0g8c1mxdmjhmlmgqp9yffgqd8vkmysdjbmawrdp3dsdfcc1iaqzi";
+    sha256 = "sha256:0j2gnsr9fd5wazx5q72ksjxdlsjzar13mmvksi5azj6p9096m73g";
   };
   apps = [
     rec {
       name = "spreed";
-      version = "16.0.0";
+      version = "16.0.4";
       url = "https://github.com/nextcloud-releases/${name}/releases/download/v${version}/${name}-v${version}.tar.gz";
-      sha256 = "sha256:0difhw865vpf020c5yvxsw699glayyf77a8xvlwlnnzr1v3wpfx6";
+      sha256 = "sha256:09qkj42nnklglm67fvmrcr7nh5xsv92a66a7jkpsirjgjnqkqrxw";
     }
   ];
 

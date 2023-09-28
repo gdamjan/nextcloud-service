7475a2f1ca4a28e3b07190bd2acdd8657fa0bf24
Дамјан Георгиевски <gdamjan@gmail.com>
2023-09-28 22:59:11 +0200

    migrate to nix flakes and use nixpkgs nextcloud
    flakes makes easier to lock the nix pkgs repo too.
    use nextcloud from nixpkgs and don't bother updating it, just
    `nix flake update` is enough

--- files/portable.config.php
+++ files/portable.config.php
@@ -1,13 +1,23 @@
 <?php
 # Nextcloud supports multiple config files, that override the generated config.php
+# https://docs.nextcloud.com/server/stable/admin_manual/configuration_server/config_sample_php_parameters.html#multiple-config-php-file
+#
 # Here we specify the configuration that's specific when running in a portable
 # service.
-# https://docs.nextcloud.com/server/stable/admin_manual/configuration_server/config_sample_php_parameters.html#multiple-config-php-file
-$STATE_DIRECTORY = explode(":", $_ENV["STATE_DIRECTORY"])[0];
-$LOGS_DIRECTORY = explode(":", $_ENV["LOGS_DIRECTORY"])[0];
+# - use systemd STATE_DIRECTORY and LOGS_DIRECTORY env variables to set
+#   datadirectory and logfile locations
+# - setup 2 readable and 1 writable apps directory:
+#   one for the apps that ship with nextcloud
+#   one for any additional apps in this package
+#   and last a writable path inside STATE_DIRECTORY for apps installed from the
+#   store by the admin
+
+$STATE_DIRECTORY = explode(":", getenv("STATE_DIRECTORY"))[0];
+$LOGS_DIRECTORY = explode(":", getenv("LOGS_DIRECTORY"))[0];
+
 $CONFIG = array (
-  'datadirectory' =>  $STATE_DIRECTORY . '/data',
-  'logfile' => $LOGS_DIRECTORY . '/nextcloud.log',
+  'datadirectory' => $STATE_DIRECTORY . '/data',
+  'logfile'       => $LOGS_DIRECTORY . '/nextcloud.log',
   'memcache.local' => '\\OC\\Memcache\\APCu',
   'apps_paths' => [
     [
---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = ''Portable Nextcloud service run by uwsgi-php and built with Nix'';
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
+      packages.x86_64-linux.default = (import ./build.nix { inherit pkgs; withSystemd = false; });
+    };
+}

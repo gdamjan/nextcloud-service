0ed5305dd56d66930a13a19a773cfcc2f956b92b
Дамјан Георгиевски <gdamjan@gmail.com>
2023-12-16 16:02:48 +0100

    add release info
    the flake now has another package output `release-info` that will report
    the versions it was built with (php, uwsgi, nixos and nextcloud).
    
    bump github actions too

--- flake.nix
+++ flake.nix
@@ -6,7 +6,15 @@
   outputs = { self, nixpkgs, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
+      nixos = (nixpkgs.lib.nixosSystem { system = "x86_64-linux"; modules = []; }).config.system.nixos;
     in {
       packages.x86_64-linux.default = (import ./build.nix { inherit pkgs; withSystemd = false; });
+      packages.x86_64-linux.release-info = pkgs.writeText "release-info.txt" ''
+        php: ${pkgs.php.version}
+        uwsgi: ${pkgs.uwsgi.version}
+        nextcloud: ${pkgs.nextcloud27.version}
+        ${nixos.distroName} ${nixos.release} (${nixos.codeName})
+        ${nixos.distroId} ${nixos.label}
+      '';
     };
 }
